c5e2faf4392a5f35b869b75382457177f3b53711
fairuzridzmanarashid <fairuzridzman.arashid@dyson.com>
2026-05-25 15:49:08 +0800

    Update attendance_web.py

--- attendance_web.py
+++ attendance_web.py
@@ -75,8 +75,8 @@ def mark_attendance(uid):
     today, today_day = get_today()
     team = user["team"]
 
-    # ✅ CORRECT LOGIC (THIS IS THE REAL FIX)
-    if today_day in  TEAM_SCHEDULE[team] = "1"
+    # ✅ FINAL CORRECT LOGIC
+    if today_day in TEAM_SCHEDULEstatus = "1"
     else:
         status = "OFF"
 
